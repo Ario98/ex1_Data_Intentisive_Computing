8e5ed92ffd39a7e19dba1be59b4c3aaeab22ddc1
Ario98 <bliteum@protonmail.com>
2022-04-16 16:34:04 +0200

    Fixed the preprocess methods as they were not iterating properly, since a deletion would mess up the indexing. Used a copy of list for iterating. Removed the remove_commas function as it was not needed anymore.

--- ex_one.py
+++ ex_one.py
@@ -1,5 +1,3 @@
-from mrjob.job import MRJob
-from mrjob.step import MRStep
 import json
 import preprocess
 
@@ -8,11 +6,12 @@ with open('dataset_smaller.json') as f:
     data = [json.loads(line) for line in f]
 
 """Initializing the preprocess object"""
-Preprocessor = preprocess.PreProcessing()
+prep = preprocess.PreProcessing()
 
 """Execution of preprocessing on reviews_dataset data"""
-data = Preprocessor.tokenize_data(data)
-data = Preprocessor.casefold_data(data)
-data = Preprocessor.stopwords_data(data)
-data = Preprocessor.remove_delimiters(data)
+data = prep.tokenize_data(data)
+data = prep.casefold_data(data)
+data = prep.stopwords_data(data)
+data = prep.remove_delimiters(data)
+
 print(data)
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,3 @@
-from mimetypes import init
 import nltk
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
@@ -14,50 +13,44 @@ class PreProcessing():
         global stopwords
         stopwords = f.read().splitlines()
 
-    """Read the stopwords file"""
-    with open('stopwords.txt') as f:
+    """Read the delimiters file. This file, however, will have to be turned into code."""
+    with open('delimiters.txt') as f:
         global delimiters
         delimiters = f.read().splitlines()
 
-    def tokenize_data(self, list):
-        self.list = list
-        for i in self.list:
+    """Tokenization to unigrams using the nltk package"""
+    def tokenize_data(self, provided_list):
+        self.provided_list = provided_list
+        for i in self.provided_list:
             for key, value in i.items():
                 i[key] = word_tokenize(str(i[key]))
-        return self.list
+        return self.provided_list
 
-    def casefold_data(self, list):
-        self.list = list
-        for i in self.list:
+    """Simple casefold of data by using string.casefold()"""
+    def casefold_data(self, provided_list):
+        self.provided_list = provided_list
+        for i in self.provided_list:
             for key, values in i.items():
                 for x in range(len(values)):
                     values[x] = values[x].casefold()
-        return self.list
+        return self.provided_list
 
-
-    def stopwords_data(self, list):
-        self.list = list
-        for i in self.list:
+    """Does it need to be changed as below since it is provided by the course?"""
+    def stopwords_data(self, provided_list):
+        self.provided_list = provided_list
+        for i in self.provided_list:
             for key, values in i.items():
-                for word in values:
+                for word in list(values):
                     if (word in stopwords) or (len(word) == 1):
                         values.remove(word)
-        return self.list
+        return self.provided_list
 
-    def remove_commas(self, list):
-        self.list = list
-        for i in self.list:
+    """This function will have to be changed so it does not need the delimiter.txt file"""
+    def remove_delimiters(self, provided_list):
+        self.provided_list = provided_list
+        for i in self.provided_list:
             for key, values in i.items():
-                for word in values:
-                    if word == ',':
-                        values.remove(word)
-        return self.list
-
-    def remove_delimiters(self, list):
-        self.list = list
-        for i in self.list:
-            for key, values in i.items():
-                for word in values:
+                for word in list(values):
                     if any(x in word for x in delimiters):
                         values.remove(word)
-        return self.list
+        return self.provided_list
